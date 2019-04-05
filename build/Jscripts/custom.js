0a4211d8cd70e0f4d7e650e12b88b41f10a3435e
longht <longht@vng.com.vn>
2019-04-05 15:56:47 +0700

    HTML-Moonji
    Release

---
+++ build/Jscripts/custom.js
@@ -0,0 +1,57 @@
+/* ====================================
+   Onload functions
+   ==================================== */
+$(function() {
+    // Start home slideshow
+    var cycle = $('.site-index .slideshow').cycle({
+        fx: 'scrollHorz',
+        speed: 800,
+        timeout: 20000,
+        slides: '> .slide',
+        allowWrap: true,
+        manualTrump: false,
+        autoHeight: false,
+        slideActiveClass: 'active',
+        next: '> .next',
+        prev: '> .prev'
+    });
+    // load image slide
+    cycle.on('cycle-before', function(event, optionHash, outgoingSlideEl, incomingSlideEl, forwardFlag) {
+        var slide = jQuery(incomingSlideEl);
+        var newSrc = slide.data('src');
+
+        $('.slide-stage').css('background-image', 'url(' + newSrc + ')')
+    });
+    // Mouse control
+    $('body.home').scrollsteps({
+        up: function() {
+            cycle.cycle('prev');
+            cycle.cycle('pause');
+        },
+        down: function() {
+            cycle.cycle('next');
+            cycle.cycle('pause');
+        }
+    });
+    $('body.about').scrollsteps({
+        up: function() {
+            cycle.cycle('prev');
+            cycle.cycle('pause');
+        },
+        down: function() {
+            cycle.cycle('next');
+            cycle.cycle('pause');
+        }
+    });
+    // affix for blog page
+    $('.blog #site-header').affix({
+        offset: {
+            top: $('.blog #site-header').outerHeight(),
+        }
+    });
+    // lightbox
+    $('.work-wrap').lightGallery({
+        selector: '.work-item',
+        videojs: true
+    });
+});
